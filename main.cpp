02f381d50cfac2639cc8500eab3e90b83895f2cf
Dmytro Varchuk <loltop792@gmail.com>
2024-11-28 21:52:04 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Calculation of the volume of the parallelepiped." << endl;	
+	cout << "Enter the length of the parallelepiped: ";	
+	double a;	
+	cin >> a;	
+	cout << "Enter the width of the parallelepiped: ";	
+	double b;	
+	cin >> b;	
+	cout << "Enter the height of the parallelepiped: ";	
+	double c;
+	cin >> c;
+	cout << "The volume of the parallelepiped is equal to: " << a * b * c << "cubic. cm." << endl;
+
+
+
+
+
+	return 0;
+
+}
